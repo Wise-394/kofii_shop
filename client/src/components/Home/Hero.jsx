1fae506cdf137c51824cdd78c0a783c1e3167f86
wise <114738222+Wise-394@users.noreply.github.com>
2026-06-06 22:17:52 +0800

    fix(Hero): update grid classes for improved responsiveness

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,18 +1,18 @@
 export function Hero() {
   return (
     <section
-      className="h-[90vh] flex flex-col sm:grid sm:grid-cols-2 justify-center
+      className="h-[90vh] flex flex-col md:grid md:grid-cols-2 justify-center
         items-center"
     >
       <div
         className="text-center flex justify-center items-center flex-col order-2
-          sm:order-1"
+          md:order-1"
       >
         <h1>Everyone loves coffee </h1>
         <p>lorem ipsum sample test that says something</p>
         <button>Buy Now!</button>
       </div>
-      <div className="order:2 sm:order-1">
+      <div className="order:2 md:order-1 items-center justify-center flex">
         <p>sample image</p>
       </div>
     </section>
