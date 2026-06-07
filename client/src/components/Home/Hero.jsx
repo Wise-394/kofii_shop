30aa0e41d5659e55f1efb39382b53ed41ea51d4c
wise <114738222+Wise-394@users.noreply.github.com>
2026-06-07 10:51:32 +0800

    style(Hero): enhance layout and improve text styling for better presentation

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -3,17 +3,22 @@ export function Hero() {
     <section
       className="h-[92vh] flex flex-col md:grid md:grid-cols-2 justify-center
         items-center bg-[url('/assets/heroBg.webp')] bg-fixed bg-center
-        bg-no-repeat bg-cover text-white"
+        bg-no-repeat bg-cover text-white relative p-2 md:p-10"
     >
+      <div className="absolute inset-0 bg-black/40" />
       <div
         className="text-center flex justify-center items-center flex-col order-2
-          md:order-1"
+          md:order-1 z-10 gap-3 md:text-left md:items:start"
       >
-        <h1>Slow down. Sip something warm.</h1>
-        <p>A cozy spot made for long conversations and quiet mornings alike.</p>
+        <h1 className="text-3xl font-bold md:text-4xl">
+          Slow down. Sip something warm.
+        </h1>
+        <p className="text-md font-semibold md:text-xl">
+          A cozy spot made for long conversations and quiet mornings alike.
+        </p>
         <button>Buy Now!</button>
       </div>
-      <div className="order:2 md:order-1 items-center justify-center flex"></div>
+      <div className="order:2 md:order-1 items-center justify-center flex z-10"></div>
     </section>
   );
 }
