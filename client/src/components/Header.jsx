fb53e02ec10bb8980e75f831a70eb6089a46c01f
wise <114738222+Wise-394@users.noreply.github.com>
2026-06-07 10:18:00 +0800

    Refactor code structure for improved readability and maintainability

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,25 +9,26 @@ export function Header() {
   };
   return (
     <header
-      className="sticky top-0 flex w-screen justify-between px-4 py-5 sm:px-10"
+      className="sticky top-0 flex w-screen justify-between px-4 py-5 sm:px-10
+        h-[8vh] bg-brown-100"
     >
       <Link
         to="/"
         className="flex items-center justify-center text-lg font-bold
-          text-orange-400"
+          text-brown-200"
       >
         <GiCoffeeCup className="" />
         Kofii Shop
       </Link>
 
       <nav className="hidden gap-15 sm:flex">
-        <Link to="/" className="hover:text-orange-400">
+        <Link to="/" className="hover:text-brown-500 font-medium">
           Home
         </Link>
-        <Link to="/cart" className="hover:text-orange-400">
+        <Link to="/cart" className="hover:text-brown-500 font-medium">
           Cart
         </Link>
-        <Link to="/shop" className="hover:text-orange-400">
+        <Link to="/shop" className="hover:text-brown-500 font-medium">
           Shop
         </Link>
       </nav>
@@ -38,19 +39,19 @@ export function Header() {
       {/* Mobile menu */}
       <nav
         className={`${isOpen ? "translate-x-0" : "translate-x-full"} flex fixed
-          top-0 right-0 h-screen w-2/4 flex-col items-center gap-10
-          bg-orange-300 pt-12 sm:hidden transition-transform`}
+          top-0 right-0 h-screen w-2/4 flex-col items-center gap-10 bg-brown-500
+          text-white pt-12 sm:hidden transition-transform`}
       >
         <button className="absolute top-5 right-5" onClick={toggleMobileMenu}>
           X
         </button>
-        <Link to="/" className="hover:text-white">
+        <Link to="/" className="hover:text-brown-200">
           Home
         </Link>
-        <Link to="/cart" className="hover:text-white">
+        <Link to="/cart" className="hover:text-brown-200">
           Cart
         </Link>
-        <Link to="/shop" className="hover:text-white">
+        <Link to="/shop" className="hover:text-brown-200">
           Shop
         </Link>
       </nav>
--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,20 +1,19 @@
 export function Hero() {
   return (
     <section
-      className="h-[90vh] flex flex-col md:grid md:grid-cols-2 justify-center
-        items-center"
+      className="h-[92vh] flex flex-col md:grid md:grid-cols-2 justify-center
+        items-center bg-[url('/assets/heroBg.webp')] bg-fixed bg-center
+        bg-no-repeat bg-cover text-white"
     >
       <div
         className="text-center flex justify-center items-center flex-col order-2
           md:order-1"
       >
-        <h1>Everyone loves coffee </h1>
-        <p>lorem ipsum sample test that says something</p>
+        <h1>Slow down. Sip something warm.</h1>
+        <p>A cozy spot made for long conversations and quiet mornings alike.</p>
         <button>Buy Now!</button>
       </div>
-      <div className="order:2 md:order-1 items-center justify-center flex">
-        <p>sample image</p>
-      </div>
+      <div className="order:2 md:order-1 items-center justify-center flex"></div>
     </section>
   );
 }
